2d10729bc98ad72bd6d08c526a4cf6726e336f6d
Savannah <savannah.l.portilla@gmail.com>
2019-02-20 16:34:37 -0500

    part 2 15min in

---
+++ server/src/models/User.js
@@ -0,0 +1,9 @@
+module.exports = {sequelize, DataTypyes} =>
+  sequelize.define('User', {
+      email: {
+          type: DataType.STRING,
+          unique: true
+      },
+      password: DataTypes.STRING
+  })
+  
