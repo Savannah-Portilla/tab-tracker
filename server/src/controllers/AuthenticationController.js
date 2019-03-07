c912eb3647401805f1f896866efa7f7a5f7eda64
Savannah <savannah.l.portilla@gmail.com>
2019-03-07 14:47:42 -0500

    trying to switch to part2 branch

--- server/src/controllers/AuthenticationController.js
+++
@@ -1,8 +0,0 @@
-//where you have all your end points defined
-module.exports = {
-    register (req, res) {
-        res.send({
-            message: 'Hello ${req.body.email}! Your user was registered! Have fun!'
-        })
-    }
-}
