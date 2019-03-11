b7aa44fe5a3042be002b23240e44f061232208e6
Savannah <savannah.l.portilla@gmail.com>
2019-03-11 15:56:26 -0400

    Professor Plokin assisted updates and fixes

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,7 @@ app.use(cors())
 
 app.post('/register', (req, res) => {
   res.send({
-    message: 'Hello ${req.body.email}! Your user was registered! Have fun!'
+    message: `Hello ${req.body.email}! Your user was registered! Have fun!`
   })
 })
 
